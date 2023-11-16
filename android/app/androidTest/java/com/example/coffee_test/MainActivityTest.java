f5843c3139f1c66a58a25e08dd9e4ff955644227
Ahmad Mukhlis Saputra <ahmad.mukhlish.saputra@gmail.com>
2023-11-17 00:53:58 +0700

    add package name

--- android/app/androidTest/java/com/example/coffee_test/MainActivityTest.java
+++ android/app/androidTest/java/com/example/coffee_test/MainActivityTest.java
@@ -5,20 +5,9 @@ import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 
-// import MainActivity from our main app
-import com.example.error_reporting_features_in_a_flutter_app.MainActivity;
-
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
   @Rule
   public ActivityTestRule<MainActivity> rule = 
   new ActivityTestRule<>(MainActivity.class, true, false);
 }
-
-
-
-
-
-
-
-
