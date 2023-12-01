00f1392d6153859d99f0cd3fd595001f82acb2e2
Ahmad Mukhlis Saputra <ahmad.mukhlish.saputra@gmail.com>
2023-12-01 23:23:38 +0700

    adding more comments

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -7,22 +7,29 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
+  //setup the integration test
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets(
     "Loading -> tab login button -> see login screen",
     (WidgetTester tester) async {
+      //loads the app
       app.main();
+
+      //rebuild the widget
       await tester.pump();
 
+      //test the widget - must be found exactly one
       expect(find.byType(SplashScreen), findsOneWidget);
 
+      //pumps until the last frame for duration
       await tester.pumpAndSettle(const Duration(seconds: 3));
 
       final homeLoginButton = find.byKey(const Key('loginButton'));
 
       expect(homeLoginButton, findsOneWidget);
 
+      //tap this widget
       await tester.tap(homeLoginButton);
 
       await tester.pumpAndSettle();
@@ -44,6 +51,8 @@ void main() {
 
       Finder emailField = find.byKey(const Key('email'));
       expect(emailField, findsOneWidget);
+
+      //enter text in the field
       await tester.enterText(emailField, 'ahmad@gmail.com');
 
       Finder passwordField = find.byKey(const Key('password'));
@@ -54,7 +63,9 @@ void main() {
 
       await tester.pumpAndSettle(const Duration(seconds: 5));
 
+      //should not find error by showing snackbar
       expect(find.byType(SnackBar), findsNothing);
+
       expect(find.byType(MenuScreen), findsOneWidget);
     },
   );
