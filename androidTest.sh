44dbce92eccae71a3790c4994bb59c8987faaf85
Ahmad Mukhlis Saputra <ahmad.mukhlish.saputra@gmail.com>
2023-12-02 00:07:16 +0700

    simplyfy androidTest.sh

--- androidTest.sh
+++ androidTest.sh
@@ -4,22 +4,9 @@ flutter build apk --debug
 ./gradlew app:assembleAndroidTest
 ./gradlew app:assembleDebug -Ptarget=integration_test/app_test.dart
 
-
-#./gradlew app:connectedAndroidTest -Ptarget=`pwd`/../integration_test/app_test.dart
 popd
 
-#gcloud auth activate-service-account --key-file=wiredbraincoffee-a1386-4485fb96c69f.json
-#
-#gcloud --quiet config set project wiredbraincoffee-a1386
-#
-#gcloud firebase test android run --type instrumentation \
-#  --app build/app/outputs/apk/debug/app-debug.apk \
-#  --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
-#  --timeout 2m \
 
-
-  # --results-bucket=<RESULTS_BUCKET> \
-  # --results-dir=<RESULTS_DIRECTORY>
 
 
 
